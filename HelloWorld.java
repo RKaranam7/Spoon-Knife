1e749e5f1ad8bcdbc810976dc6c7809bc0b1d75d
RKaranam7 <ramu.karanam7@gmail.com>
2019-01-20 16:41:03 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test.week1;
+
+/**
+ * @author kvssw
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
